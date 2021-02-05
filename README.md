f9ca79d1c80e3978d640ab69c4f12e3bd3529f7e
KhadijaHasanova <khadijahasanova28@gmail.com>
2021-02-05 10:36:26 -0600

    Added images

---
+++ README.md
@@ -0,0 +1,4 @@
+# Car Racing Stage 2
+Add a track in the background.
+Replace the car sprites with images of real cars.
+Write a condition to end the game.
--- sketch.js
+++ sketch.js
@@ -10,6 +10,20 @@ var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
+var track;
+
+var car1_img, car2_img, car3_img, car4_img;
+
+function preload() {
+  //load the images for the cars
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+
+  //load image for the track
+  track = loadImage("images/track.jpg");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -24,8 +38,13 @@ function draw(){
   if(playerCount === 4){
     game.update(1);
   }
+  
   if(gameState === 1){
     clear();
     game.play();
+  }
+
+  if(gameState === 2) {
+    game.end();
   }
 }
